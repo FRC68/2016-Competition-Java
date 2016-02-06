553cee9000c7e48a23792006192c653a34fd69af
James Michniewicz <jmicz3d@gmail.com>
2016-02-06 13:15:40 -0500

    Remove four test comments

--- src/org/usfirst/frc/team68/robot/Robot.java
+++ src/org/usfirst/frc/team68/robot/Robot.java
@@ -20,12 +20,7 @@ public class Robot extends IterativeRobot {
 
 //	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
-	
-	// This is a comment 
-	// This is a second comment
-	// This is a third comment
-	// This is a fourth comment
-	
+
     Command autonomousCommand;
     SendableChooser chooser;
 
