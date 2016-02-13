d906963c9f09fdfbfb29a3c38e6a0cc712e04813
MatthewEckert <68gitmatte@gmail.com>
2016-02-13 10:14:29 -0500

    Add constants for xbox controller, shooter pneumatics, air pump and intake motor.

--- src/org/usfirst/frc/team68/robot/RobotMap.java
+++ src/org/usfirst/frc/team68/robot/RobotMap.java
@@ -21,10 +21,35 @@ public class RobotMap {
     public static final int ARM_BASE_MOTOR = 7;  // CAN bus ID 7
     public static final int ARM_SHOULDER_MOTOR = 8; // CAN bus ID 8
     public static final int ARM_ELBOW_MOTOR = 9; // CAN bus ID 9
+    
+    //Intake Port Mapping Constants
+    public static final int INTAKE_MOTOR = 10;  // CAN bus ID 10
+    
+    // Can Holder Port Mapping Constants
+    public static final int HOOD_FORWARD = 0;
+    public static final int HOOD_REVERSE = 1;
 
- // Joystick Port Mapping Constants
+    // Joystick Port Mapping Constants
     public static final int LEFT_JOYSTICK = 0;			// USB port 0
     public static final int RIGHT_JOYSTICK = 1;			// USB port 1
     public static final int JOYSTICK_Y = 1;				// This works for both joysticks
+    
+    // xBoxController Button & Axis Mapping Constants
+    // Buttons
+    public static final int XBOX_CONTROLLER = 2;		// USB port 2
+    public static final int XBOX_A = 1;
+    public static final int XBOX_B = 2;
+    public static final int XBOX_X = 3;
+    public static final int XBOX_Y = 4;
+    public static final int XBOX_LB = 5;
+    public static final int XBOX_RB = 6;
+    // Axis
+    public static final int XBOX_LY = 1;
+    public static final int XBOX_RY = 5;
+
+    
+    // Pneumatics Port Mapping Constants
+    public static final int PCM_MAIN = 0;
+  
 }
 
