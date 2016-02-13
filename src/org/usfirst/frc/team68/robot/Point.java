d9504f1b9317bc668a8ab1cdbc3df656aaddc27d
James Michniewicz <jmicz3d@gmail.com>
2016-02-13 14:06:50 -0500

    Add Point and PointPath classes.

---
+++ src/org/usfirst/frc/team68/robot/Point.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team68.robot;
+
+public class Point {
+	public double x, y, z;
+
+	public String toString(){
+		return "(x:" + x +" y:" + y + " z:" + z + ")";
+	}
+}
---
+++ src/org/usfirst/frc/team68/robot/PointPath.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team68.robot;
+
+import java.awt.PointerInfo;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PointPath {
+	private List<Point> points = new ArrayList<Point>();
+	
+	public Point getPointAtPercent(double percent){
+		int index = (int) Math.round((points.size()/100)*percent) -1; //Minus one b/c array index starts at and size is the number of elements
+		if(index > points.size())
+			index = points.size();
+		
+		return points.get(index);
+	}
+	
+	public void addPoint(int x, int y, int z){
+		Point ins = new Point();
+		ins.x = x; ins.y = y; ins.z = z;
+		points.add(ins);
+	}
+	
+	public void addPoint(int index, int x, int y, int z){
+		Point ins = new Point();
+		ins.x = x; ins.y = y; ins.z = z;
+		points.add(index, ins);
+	}
+	
+	public void removePoint(int index){
+		points.remove(index);
+	}
+	
+	public String toString(){
+		StringBuilder retval = new StringBuilder();
+		
+		for(int i = 0; i < points.size()-1; i++){
+			retval.append(points.get(i).toString());
+			retval.append("\n");
+		}
+		return retval.toString();
+	}
+	
+}
