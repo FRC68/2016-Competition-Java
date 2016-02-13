0f4e3e080d166d577f02d5feea1893122ff97336
Nick R <nroberts19@users.brandonschooldistrict.org>
2016-02-13 16:43:04 -0500

    More code for intake, stop code and a set speed code

--- src/org/usfirst/frc/team68/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team68/robot/subsystems/Intake.java
@@ -1,19 +1,61 @@
 
 package org.usfirst.frc.team68.robot.subsystems;
 
+import org.usfirst.frc.team68.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Intake extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	
+	private CANTalon intakeMotor;
+	private CANTalon intakeArm;
+	
+	private static Intake intake;
+	
+	public static Intake getIntake() {
+		if (intake == null)  {
+			intake = new Intake();
+		}
+		return intake;
+	}
+	
+	private Intake() {
+    	intakeMotor = new CANTalon(RobotMap.INTAKE_MOTOR);
+    	//intakeArm = new CANTalon(RobotMap.INTAKE_ARM);
+    }
 
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
+        //Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    
+    public void setIntakeSpeed(double speed) {
+    	intakeMotor.set(speed);
+    }
+    
+    public double getIntakeSpeed() {
+    	return intakeMotor.get();
+    	
+    }
+    
+    
+    public static void getSpeed() {
+	}
+    
+    public void intakeWithXboxJoysticks (double speedXboxJoystickValue) {
+    	this.setIntakeSpeed (speedXboxJoystickValue) ;
+    	
+    }
+    
+    public void stopIntakeMotor (double speed) {
+    	this.setIntakeSpeed(0) ;
+    } 
+    
+    public void setIntakeArm() {
+    	
+    }
 }
-
