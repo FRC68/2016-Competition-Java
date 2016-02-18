344f47ed6ba5eceab9ee175a82ce340ab16b9906
James Michniewicz <jmicz3d@gmail.com>
2016-02-17 19:12:25 -0500

    Change RobotMap references in Intake.java

--- src/org/usfirst/frc/team68/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team68/robot/subsystems/Intake.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Intake extends Subsystem {
 	
-	private CANTalon intakeMotor;
+	private CANTalon intakeRoller;
 	private CANTalon intakeArm;
 	
 	private static Intake intake;
@@ -24,8 +24,8 @@ public class Intake extends Subsystem {
 	}
 	
 	private Intake() {
-    	intakeMotor = new CANTalon(RobotMap.INTAKE_MOTOR);
-    	//intakeArm = new CANTalon(RobotMap.INTAKE_ARM);
+    	intakeRoller = new CANTalon(RobotMap.INTAKE_ROLLER_MOTOR);
+    	intakeArm = new CANTalon(RobotMap.INTAKE_ARM_MOTOR);
     }
 
     public void initDefaultCommand() {
@@ -34,11 +34,11 @@ public class Intake extends Subsystem {
     }
     
     public void setIntakeSpeed(double speed) {
-    	intakeMotor.set(speed);
+    	intakeRoller.set(speed);
     }
     
     public double getIntakeSpeed() {
-    	return intakeMotor.get();
+    	return intakeRoller.get();
     	
     }
     
