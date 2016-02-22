89bbd7cb489df63e42d9e4fb7f76958ec00a73a7
MatthewEckert <68gitmatte@gmail.com>
2016-02-22 17:03:30 -0500

    Add CommandGroup command for shooting. The exact commands it calls still need to be worked out.

---
+++ src/org/usfirst/frc/team68/robot/commands/ShootBoulder.java
@@ -0,0 +1,25 @@
+
+package org.usfirst.frc.team68.robot.commands;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
+public class ShootBoulder extends CommandGroup {
+
+    public ShootBoulder() {
+    	// Assumes there is a boulder in the staged position in the intake
+    	// 
+    	// The following commented lines of code are a potential solution
+    	// that still needs review
+    	
+/*
+        addSequential(new RetractArmForShooting()); 
+        addSequential(new OpenHood());
+        addSequential(new SetShooterSpeed(double shootingSpeed));
+        addSequential(new VisionCommand());
+        addSequential(new FeedBoulderToShooter());
+        addSequential(new WaitCommand(3));
+        addSequential(new SetShooterSpeed(0));        
+        addSequential(new CloseHood());
+*/
+    } 
+}
