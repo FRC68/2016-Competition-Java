7f8456c6c3eb1f57b71cf3ec18d1dc683e93a0fb
Ben Eckert <ben.eckert16@gmail.com>
2016-02-27 00:11:17 -0500

    Fix parameter in call to SetShooterSpeed command

--- src/org/usfirst/frc/team68/robot/commands/ShootBoulder.java
+++ src/org/usfirst/frc/team68/robot/commands/ShootBoulder.java
@@ -1,5 +1,7 @@
 
 package org.usfirst.frc.team68.robot.commands;
+import org.usfirst.frc.team68.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
@@ -15,11 +17,10 @@ public class ShootBoulder extends CommandGroup {
         addSequential(new SetArmSafeForShooting()); 
         addSequential(new SetIntakeSafeForShooting());
         addSequential(new OpenHood());
-        addSequential(new SetShooterSpeed(double RobotMap.SHOOTER_SHOOTING_SPEED));
+        addSequential(new SetShooterSpeed(RobotMap.SHOOTER_SHOOTING_SPEED));
         addSequential(new FeedBoulderToShooter());
         addSequential(new WaitCommand(3));
         addSequential(new SetShooterSpeed(0));        
         addSequential(new CloseHood());
-
     } 
 }
